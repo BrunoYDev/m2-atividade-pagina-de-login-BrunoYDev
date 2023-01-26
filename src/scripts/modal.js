0c9b7a36b112f5f3d874ca8c5006a5f1cd8beecd
Bruno R Garcia <bruno.rogues.garcia@gmail.com>
2023-01-26 13:16:09 -0300

    Atividadezinha concluida

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,23 @@
 /* Desenvolva seu código aqui... */
+
+function handleModal(){
+    const registerBtn = document.querySelector('.btn-cadastrar');
+    const modal__controller = document.querySelector('.modal__controller');
+
+    registerBtn.addEventListener('click', () =>{
+        modal__controller.showModal();
+    })
+
+    closeModal();
+}
+
+handleModal();
+
+function closeModal(){
+    const closeBtn = document.querySelector('.closeBtn');
+    const modal__controller = document.querySelector('.modal__controller');
+
+    closeBtn.addEventListener('click', () =>{
+        modal__controller.close();
+    })
+}
